694a30c5f70db0a135732778e0db7412b2261bb1
Work <pedroiki@gmail.com>
2020-03-06 00:01:44 +0100

    Updates

---
+++ precache-manifest.68d93a82aea2ad07b64068cc59f09c5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c6d480937a12a4b67309c2ce3008fe",
+    "url": "/React_app/index.html"
+  },
+  {
+    "revision": "50df52694dfd681aa3f7",
+    "url": "/React_app/static/css/main.8f568724.chunk.css"
+  },
+  {
+    "revision": "471b743f6b35769bf24f",
+    "url": "/React_app/static/js/2.50b7c740.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React_app/static/js/2.50b7c740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50df52694dfd681aa3f7",
+    "url": "/React_app/static/js/main.c9c4f092.chunk.js"
+  },
+  {
+    "revision": "4c9a143ecd0cce68df4c",
+    "url": "/React_app/static/js/runtime-main.e77b32ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
