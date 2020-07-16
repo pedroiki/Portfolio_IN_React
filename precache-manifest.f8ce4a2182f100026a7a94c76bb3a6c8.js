efa80b81ffc0fca0e1e2d9857a6b879bdc1eb8b5
Carlos Pedro Gomes <57367344+pedroiki@users.noreply.github.com>
2020-07-17 00:15:53 +0200

    Updates

---
+++ precache-manifest.f8ce4a2182f100026a7a94c76bb3a6c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fa1f06fa47eb5b0104f188a59339cf",
+    "url": "/Portfolio_IN_React/index.html"
+  },
+  {
+    "revision": "5e765c60e2bfbe9379ed",
+    "url": "/Portfolio_IN_React/static/css/main.2867eb3c.chunk.css"
+  },
+  {
+    "revision": "1ff9dbc8cfc10ca4e19b",
+    "url": "/Portfolio_IN_React/static/js/2.691764e7.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Portfolio_IN_React/static/js/2.691764e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e765c60e2bfbe9379ed",
+    "url": "/Portfolio_IN_React/static/js/main.2e797786.chunk.js"
+  },
+  {
+    "revision": "d4f27817b237b80099c4",
+    "url": "/Portfolio_IN_React/static/js/runtime-main.8244142a.js"
+  },
+  {
+    "revision": "2b6c0845b3e29c37ef4ee838c2fc9f7d",
+    "url": "/Portfolio_IN_React/static/media/01.2b6c0845.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Portfolio_IN_React/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_app/precache-manifest.498d330a366b16fe33e4fc9b62cfd30a.js"
+  "/Portfolio_IN_React/precache-manifest.f8ce4a2182f100026a7a94c76bb3a6c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio_IN_React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
